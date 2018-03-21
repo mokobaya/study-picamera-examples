ecbbe9aecba32ca2ab0be70c84c8420292091b35
moto koba <mokobaya1@gmail.com>
2018-03-21 13:34:38 +0900

    logincss

--- camera/main.py
+++ camera/main.py
@@ -1,39 +1,34 @@
-from flask import Flask, render_template, Response
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Response
+import os
 #from processor.simple_streamer import SimpleStreamer as VideoCamera
 # from processor.pedestrian_detector import PedestrianDetector as VideoCamera
 from processor.motion_detector import MotionDetector as VideoCamera
 import time
 import threading
 
-USERNAME = 'admin'
-PASSWORD = 'default'
-
 video_camera = VideoCamera(flip=False)
 
 
 app = Flask(__name__)
 
-#---------------
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != current_app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != current_app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('flaskr.show_entries'))
-    return render_template('login.html', error=error)
-
-#---------------
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('index.html')
+ 
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'pass1234' and request.form['username'] == 'kanri':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
+ 
 def gen(camera):
     while True:
         frame = camera.get_frame()
@@ -45,5 +40,6 @@ def video_feed():
     return Response(gen(video_camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
     app.run(host='0.0.0.0', debug=False, threaded=True)
